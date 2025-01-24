f6261d94086b09c4611f4625a14b8f2218268755
erfan ashki <erfanbayern111@gmail.com>
2025-01-24 14:11:41 +0330

    string module

---
+++ password_generator.py
@@ -0,0 +1,19 @@
+import string
+
+str1 = string.ascii_letters
+str2 = string.ascii_lowercase
+str3 = string.ascii_uppercase
+str4 = string.digits
+str5 = string.hexdigits
+str6 = string.whitespace
+str7 = string.punctuation
+
+
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+print(str5)
+print(str6)
+print(str7)
+
