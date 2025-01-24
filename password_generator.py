e95b360f30e28c4e65445259f9dea450d75d53d8
erfan ashki <erfanbayern111@gmail.com>
2025-01-24 16:36:03 +0330

    implement get_settings_from_user

--- password_generator.py
+++ password_generator.py
@@ -9,3 +9,24 @@ settings = {
     'length' : 10
 }
 
+def get_settings_from_user(settings):
+    
+    for option, default in settings.items():
+        if option != 'length': 
+            while True:
+                user_setting = input(f"{option} include ? default is : {default} . (y:yes - n:no) ")
+                
+                if user_setting in ['y', 'n']:
+                    if user_setting == 'y':
+                        settings[option] = True
+                    else:
+                        settings[option] = False
+                    break
+                else:
+                    print('Invalid input , Please try again .')
+        
+    return settings
+        
+get_settings_from_user(settings)
+
+print(settings)
