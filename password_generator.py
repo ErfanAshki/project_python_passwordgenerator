fbcb9a5b86fc19639ddaf7c641552ede0388f63b
erfan ashki <erfanbayern111@gmail.com>
2025-01-24 17:29:10 +0330

    reform get_settings_from_user to get_valid_options_for_settings function

--- password_generator.py
+++ password_generator.py
@@ -9,23 +9,26 @@ settings = {
     'length' : 10
 }
 
+
+def get_valid_options_for_settings(option, default):
+    while True:
+        user_setting = input(f"{option} include ? default is : {default} . (y:yes - n:no) ")
+        
+        if user_setting in ['y', 'n']:
+            return user_setting == 'y'
+        
+        print('Invalid input , Please try again .')
+
+
 def get_settings_from_user(settings):
     
     for option, default in settings.items():
         if option != 'length': 
-            while True:
-                user_setting = input(f"{option} include ? default is : {default} . (y:yes - n:no) ")
-                
-                if user_setting in ['y', 'n']:
-                    if user_setting == 'y':
-                        settings[option] = True
-                    else:
-                        settings[option] = False
-                    break
-                else:
-                    print('Invalid input , Please try again .')
+           option , default = get_settings_from_user(option , default)
+            
         
     return settings
+
         
 get_settings_from_user(settings)
 
