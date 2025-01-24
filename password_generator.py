5a8845c9e11b5fe00287e938215210a061e81cb4
erfan ashki <erfanbayern111@gmail.com>
2025-01-24 17:41:47 +0330

    state Enter for default in get_valid_options_for_settings function

--- password_generator.py
+++ password_generator.py
@@ -12,7 +12,10 @@ settings = {
 
 def get_valid_options_for_settings(option, default):
     while True:
-        user_setting = input(f"{option} include ? default is : {default} . (y:yes - n:no) ")
+        user_setting = input(f"{option} include ? default is : {default} . (y:yes - n:no - enter:default) ")
+
+        if user_setting == '':
+            return default
         
         if user_setting in ['y', 'n']:
             return user_setting == 'y'
@@ -24,7 +27,8 @@ def get_settings_from_user(settings):
     
     for option, default in settings.items():
         if option != 'length': 
-           option , default = get_settings_from_user(option , default)
+            user_choice = get_valid_options_for_settings(option , default)
+            settings[option] = user_choice
             
         
     return settings
